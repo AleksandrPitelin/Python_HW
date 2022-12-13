4a5f6665ee317069164ebbc3e7176ed6d282522d
OleksandrPitelin <partsua@ukr.net>
2022-12-13 15:04:09 +0200

    add branch HomeWork03

---
+++ HomeWork_04/HW4.py
@@ -0,0 +1,25 @@
+
+str_or_int = input("Enter a text ")
+
+if str_or_int == int:
+    print(int("This is int"))
+else:
+    print(str("This is str"))
+
+
+
+#input("ENTER a text ")
+
+#if input("Enter a text"):
+#print("This is a integer")
+#else input() == str:
+#print("This is a string")
+
+#len_of_the_text = len(input())
+#print(len_of_the_text)
+
+#name = input("What's your name?" + " ")
+#print('Hello', name, "!")
+#age = int(input("How old are you?"))
+#print(age)
+
