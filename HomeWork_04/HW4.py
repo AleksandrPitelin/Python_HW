95890e26d2f8de1e0e9ba28a817879f969b846bc
OleksandrPitelin <partsua@ukr.net>
2022-12-15 09:10:18 +0200

    changed111 changed222

--- HomeWork_04/HW4.py
+++ HomeWork_04/HW4.py
@@ -1,14 +1,33 @@
-def question():
-    while True:
-        name = input("Назовите Ваше имя ")
-        try:
-            number = int(input("Сколько вам лет? "))
-        except: ValueError
-            print("Error! Это не число, попробуйте снова.")
-        else:
-             print("Круто, ", name, "! ,это", len(name), "букв , Вам", number*12,"Месяцев,Это", number*12%2, "число")
-             break
-question()
+some_text = input("Enter a text: ")
+if some_text.isdigit():
+    print(int(some_text) % 2 == 0)
+    print (f"{some_text} This is a digit")
+# elif some_text.isdigit():
+#     print(int(some_text) % 3 == 0)
+#     print(f'{some_text} This is not a digit')
+
+
+else: len(some_text)
+print(len(some_text))
+print("have len,this is a string")
+
+
+
+
+
+
+
+# def question():
+#     while True:
+#         name = input("Назовите Ваше имя ")
+#         try:
+#             number = int(input("Сколько вам лет? "))
+#         except: ValueError
+#             print("Error! Это не число, попробуйте снова.")
+#         else:
+#              print("Круто, ", name, "! ,это", len(name), "букв , Вам", number*12,"Месяцев,Это", number*12%2, "число")
+#              break
+# question()
 
 
 
