e87986708997624684d8e648d3aef5bcc74a0182
OleksandrPitelin <partsua@ukr.net>
2022-12-18 20:54:32 +0200

    add change HW4

--- HomeWork_04/HW4.py
+++ HomeWork_04/HW4.py
@@ -1,56 +1,9 @@
 some_text = input("Enter a text: ")
-if some_text.isdigit():
-    print(int(some_text) % 2 == 0)
-    print (f"{some_text} This is a digit")
-# elif some_text.isdigit():
-#     print(int(some_text) % 3 == 0)
-#     print(f'{some_text} This is not a digit')
-
-
-else: len(some_text)
-print(len(some_text))
-print("have len,this is a string")
-
-
-
-
-
-
-
-# def question():
-#     while True:
-#         name = input("Назовите Ваше имя ")
-#         try:
-#             number = int(input("Сколько вам лет? "))
-#         except: ValueError
-#             print("Error! Это не число, попробуйте снова.")
-#         else:
-#              print("Круто, ", name, "! ,это", len(name), "букв , Вам", number*12,"Месяцев,Это", number*12%2, "число")
-#              break
-# question()
-
-
-
-#str_or_int = input("Enter a text ")
-
-#if str_or_int == int:
-#    print(int("This is int"))
-#else:
-#    print(str("This is str"))
-
-
-
-#input("ENTER a text ")
-
-#if input("Enter a text"):
-#print("This is a integer")
-#else input() == str:
-#print("This is a string")
-
-#len_of_the_text = len(input())
-#print(len_of_the_text)
-
-#name = input("What's your name?" + " ")
-#print('Hello', name, "!")
-#age = int(input("How old are you?"))
-#print(age)
+if some_text.isalpha():
+    print(len(some_text))
+    print("have len,this is a string")
+elif some_text.isdigit():
+    if (int(some_text)) % 2 == 0:
+        print("This is a digit")
+    else:
+        print("This is not a digit")
