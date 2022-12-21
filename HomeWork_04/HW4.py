a38bf6649df0bd7e96e95442f2085523efdcec76
OleksandrPitelin <119613012+AleksandrPitelin@users.noreply.github.com>
2022-12-21 22:09:52 +0200

    HW5.py

--- HomeWork_04/HW4.py
+++ HomeWork_04/HW4.py
@@ -1,37 +1,32 @@
-def question():
-    while True:
-        name = input("Назовите Ваше имя ")
-        try:
-            number = int(input("Сколько вам лет? "))
-        except: ValueError
-            print("Error! Это не число, попробуйте снова.")
+from datetime import time
+#---------------------#
+# Home Work 1
+
+words = input("Enter a text: ")
+
+for letter in words:
+    if letter.istitle():
+        print("This is a title")
+    elif letter.islower():
+        print("This is a lower")
+    elif letter.isdigit():
+        if (int(letter)) % 2 == 0:
+            print("This is a digit")
         else:
-             print("Круто, ", name, "! ,это", len(name), "букв , Вам", number*12,"Месяцев,Это", number*12%2, "число")
-             break
-question()
+            print("This is not a digit")
+    elif letter.isspace():
+        print("This is a space")
 
 
 
-#str_or_int = input("Enter a text ")
 
-#if str_or_int == int:
-#    print(int("This is int"))
-#else:
-#    print(str("This is str"))
+#---------------------#
+# Home Work 2
 
 
+import time
 
-#input("ENTER a text ")
-
-#if input("Enter a text"):
-#print("This is a integer")
-#else input() == str:
-#print("This is a string")
-
-#len_of_the_text = len(input())
-#print(len_of_the_text)
-
-#name = input("What's your name?" + " ")
-#print('Hello', name, "!")
-#age = int(input("How old are you?"))
-#print(age)
+text = "I love Python"
+for word in text:
+    time.sleep(4.2)
+    print(text)
