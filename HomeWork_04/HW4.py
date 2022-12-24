f9925d9cfbfc158b42ebeb45d31051413f1b2835
OleksandrPitelin <119613012+AleksandrPitelin@users.noreply.github.com>
2022-12-24 17:02:20 +0200

    Update HW4.py

--- HomeWork_04/HW4.py
+++ HomeWork_04/HW4.py
@@ -1,19 +1,17 @@
-from datetime import time
-#---------------------#
-# Home Work 1
+# Home Work 5.1
 
 words = input("Enter a text: ")
 
 for letter in words:
-    if letter.istitle():
-        print("This is a title")
+    if letter.isupper():
+        print("This is a isupper")
     elif letter.islower():
         print("This is a lower")
     elif letter.isdigit():
         if (int(letter)) % 2 == 0:
-            print("This is a digit")
+            print("This is pair digit")
         else:
-            print("This is not a digit")
+            print("This is not a pair digit")
     elif letter.isspace():
         print("This is a space")
 
@@ -21,12 +19,13 @@ for letter in words:
 
 
 #---------------------#
-# Home Work 2
 
+# Home Work 5.2
 
 import time
 
-text = "I love Python"
-for word in text:
-    time.sleep(4.2)
+while True:
+    text = "I love Python"
+    for word in text:
+        time.sleep(4.2)
     print(text)
