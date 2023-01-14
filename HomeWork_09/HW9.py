d4a43600d00346d9c235dac1b1a7d68806c54d37
AleksandrPitelin <partsua@ukr.net>
2023-01-14 13:50:30 +0200

    HomeWork_09/HW9.py

--- HomeWork_09/HW9.py
+++ HomeWork_09/HW9.py
@@ -2,11 +2,21 @@
 text = int(input("Enter a digit: "))
 def my_generator():
     f1, f2 = 0, 1
-    for i in range(text):
-        print(f1, end=" ")
+    for my_generator in range(text):
+        yield
+        print(f1,end=" ")
         f1, f2 = f2, f1 + f2
-    yield i
+
 
 gen = my_generator()
 print(next(gen))
-
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
