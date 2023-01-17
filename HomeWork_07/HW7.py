c23bc86653f5a8b1c1b96a19c111025733e5aadb
AleksandrPitelin <partsua@ukr.net>
2023-01-17 15:58:48 +0200

    change HomeWork_07/HW7.py

--- HomeWork_07/HW7.py
+++ HomeWork_07/HW7.py
@@ -6,35 +6,35 @@ phonebook = {
 
     }
 
-
-if input("If you wont add - Enter 'add'") == "add":
-
-    n = input("Enter a name: ")
-    p = int(input("Enter a phone: "))
-
-    if n != phonebook:
-        phonebook[n] = p
-        print("Contact add to phonebook!")
-    else:
-        for n,p in phonebook is True:
-            print("Contact in phonebook!")
-    print(len(phonebook))
-    print(phonebook)
-elif input("If you wont del - Enter 'delete'") == "delete":
-    n = input("Enter a name: ")
-    if phonebook.get(n):
-        del phonebook[n]
-        print(phonebook)
-        print(f"{n} is delete")
-elif input("If you wont see a list - Enter a 'list' ") == "list":
-    for key in phonebook:
-        print(key)
-elif input("If you wont see a stats - Enter a 'stats' ") == "stats":
+while True:
+    command = input('Enter a command: ')
+    if command == 'list':
+        for key in phonebook:
+            print(key)
+    elif command == 'stats':
         L = phonebook
         print(len(L))
-elif input("If you wont see a show - Enter a 'show' ") == "show":
-    s = input("Enter a name: ")
-    if phonebook.get(s):
-        print(phonebook[s])
-else:
+    elif command == 'show':
+        s = input("Enter a name: ")
+        if phonebook.get(s):
+            print(phonebook[s])
+    elif command == 'add':
+        n = input("Enter a name: ")
+        p = int(input("Enter a phone: "))
+
+        if n != phonebook:
+            phonebook[n] = p
+            print("Contact add to phonebook!")
+        else:
+            for n, p in phonebook is True:
+                print("Contact in phonebook!")
+        print(len(phonebook))
+        print(phonebook)
+    elif command == 'delete':
+        n = input("Enter a name: ")
+        if phonebook.get(n):
+            del phonebook[n]
+            print(phonebook)
+            print(f"{n} is delete")
+    else:
         print("Unknown command!")
