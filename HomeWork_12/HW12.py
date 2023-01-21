ab5daf9a21489030ff49b974f7f108d8454c50aa
AleksandrPitelin <partsua@ukr.net>
2023-01-21 18:14:18 +0200

    cangedir HomeWork_12/HW12.py

--- HomeWork_12/HW12.py
+++ HomeWork_12/HW12.py
@@ -6,9 +6,11 @@ phonebook = {
         "Aleks": "347638756",
         "Fedor": "3784638754",
         "Den": "2387463745"
-
     }
-# with open("HomeWork_12/HW12.py", "w")
 
 json_phonebook = json.dumps(phonebook)
 print(type(json_phonebook))
+print(json_phonebook)
+
+with open("HomeWork_12/Json_hw12", "x") as file_Json:
+    file_Json.write(json_phonebook)
