d2bc0fe16a1dd0455a949202f3ca18efba90f206
AleksandrPitelin <partsua@ukr.net>
2023-01-28 10:44:03 +0200

    add change HomeWork_12/HW12.py

--- HomeWork_12/HW12.py
+++ HomeWork_12/HW12.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 def timer():
     start = datetime()
@@ -10,20 +11,29 @@ def funcname(func):
         print(f"Функція {func.__name__}")
         return func(*args,**kwargs)
 
-import json
 
 
 
-phonebook = {
-        "Aleks": "347638756",
-        "Fedor": "3784638754",
-        "Den": "2387463745"
 
-    }
-json_phonebook = json.dumps(phonebook)
+# phonebook ={
+#         "Aleks": "347638756",
+#         "Fedor": "3784638754",
+#         "Den": "2387463745"
+#
+#      }
+# json_phonebook = json.dumps(phonebook)
+
+try:
+    with open("/Users/AleksandrPitelin/PythonProject/PythonProject/HomeWork_12/Json_hw12", 'r') as file:
+        data = file.read()
+        phonebook = json.loads(data)
+except FileNotFoundError:
+    phonebook = {}
+
+
 while True:
-    @timer
-    @funcname:
+    # @timer(command)
+    # @funcname(command)
     command = input('Enter a command: ')
     if command == 'list':
         for key in phonebook:
@@ -39,10 +49,13 @@ while True:
         n = input("Enter a name: ")
         p = int(input("Enter a phone: "))
 
-        if n != phonebook:
+        # if n != phonebook:
+        #     phonebook[n] = p
+        if n not in phonebook:
             phonebook[n] = p
-            with open("/Users/AleksandrPitelin/PythonProject/PythonProject/HomeWork_12/Json_hw12", "w") as file_Json:
-                file_Json.write(json_phonebook)
+            with open("/Users/AleksandrPitelin/PythonProject/PythonProject/HomeWork_12/Json_hw12", 'w+') as file_json:
+                content = json.dumps(phonebook)
+                file_json.write(content)
             print("Contact add to phonebook!")
         else:
             for n, p in phonebook is True:
@@ -53,8 +66,6 @@ while True:
         n = input("Enter a name: ")
         if phonebook.get(n):
             del phonebook[n]
-            with open("/Users/AleksandrPitelin/PythonProject/PythonProject/HomeWork_12/Json_hw12", "w") as file_Json:
-                file_Json.write(json_phonebook)
             print(phonebook)
             print(f"{n} is delete")
     else:
