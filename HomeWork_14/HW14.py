6b3afdb8782097f39e78209e754c7562bb104d5b
AleksandrPitelin <partsua@ukr.net>
2023-02-01 13:18:27 +0200

    add change HomeWork_14/HW14.py

--- HomeWork_14/HW14.py
+++ HomeWork_14/HW14.py
@@ -1,13 +1,5 @@
 import re
-numphone1 = "+380"
-numphone2 = "380"
-numphone3 = "0"
-for i in range (4):
-    print(i)
-if numphone1 is True:
-    print(re.findall('\+380',numphone1))
-elif numphone2 is True:
-    print(re.findall('380',numphone2))
-elif numphone3 is True:
-    print(re.findall('0',numphone3))
-else:print(ValueError)
+
+tel = re.search(r'\+380'[0-4],input('enter a text: '))
+
+print(tel[0] if tel else "Not found")
