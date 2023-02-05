c429e590873723c7893801d8153cf9e6a16dcdbb
AleksandrPitelin <partsua@ukr.net>
2023-02-05 14:21:21 +0200

    HomeWork_18/HW18.py

---
+++ HomeWork_18/HW18.py
@@ -0,0 +1,10 @@
+class Bot:
+    def __init__(self,name):
+        self.name = name
+        print("name")
+    # def say_name(self):
+    #     super().__init__(name)
+
+    # def send_messege(self):
+
+Bot_name = Bot("Aleksandr")
