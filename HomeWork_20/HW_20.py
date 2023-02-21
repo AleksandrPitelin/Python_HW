a52d4270285f6bc4360c768e8ea61f7677231116
AleksandrPitelin <partsua@ukr.net>
2023-02-21 16:59:10 +0200

    add changes

--- HomeWork_20/HW_20.py
+++ HomeWork_20/HW_20.py
@@ -1,9 +1,8 @@
-import sqlite3
+
 
 db = sqlite3.connect("/Users/AleksandrPitelin/PythonProject/PythonProject/HomeWork_20/robot.sqlite")
-#
-# c = db.cursor()
+c = db.cursor()
+
 
 # db.commit()
 # db.close()
-
