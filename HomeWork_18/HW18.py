5d0dd289f6673bc2e51d5f98bb2f475e8af96ec3
AleksandrPitelin <partsua@ukr.net>
2023-03-01 19:14:49 +0200

    update HomeWork_18/HW18.py

--- HomeWork_18/HW18.py
+++ HomeWork_18/HW18.py
@@ -18,18 +18,18 @@ class TelegramBot(Bot):
     def __init__(self,url,chat_id):
         self.url = url
         self.chat_id = chat_id
+
     def set_url(self):
         print(f"my url : {self.url}")
     def set_chat_id(self):
         print(f"My ID is: {self.chat_id}")
-
     def send_message(self):
-        super.__init__(self.message,self.name)
         print(f"{self.message},{self.name} and i have {self.url} with {self.chat_id} ")
 
 
 
 url = TelegramBot("https://www.gismeteo.ua/weather-dnipro-5077/","weazer")
 chat = TelegramBot("id_number bla-bla-bla","535366")
+
 url.set_url()
 chat.set_chat_id()
