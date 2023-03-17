60286e3e3b96d4479cc97b24a95de12a8e009a54
AleksandrPitelin <partsua@ukr.net>
2023-03-17 13:50:17 +0200

    add HW27

---
+++ HomeWork_27/document.js
@@ -0,0 +1,66 @@
+const myDiv = document.createElement('div');
+myDiv.className = 'buttons';
+myDiv.style.background = 'purple';
+myDiv.style.textAlign = 'center';
+
+let i=0;
+
+
+['Додати в друзі','Написати повідомлення ','Запропонувати роботу'].map(buttonName => {
+    let button = document.createElement('button');
+    button.className = 'btn btn-success';
+    button.innerText = `${buttonName} ${i}`;
+    button.style.margin = '5px'
+    myDiv.appendChild(button);
+    })
+document.getElementsByTagName('body')[0].appendChild(myDiv);
+
+
+['Кiлькiсть друзiв'].map(buttonName => {
+    let button2 = document.createElement('button2');
+    button2.className = 'btn btn-success';
+    button2.innerText = `${buttonName} ${i}`;
+    button2.style.margin = '5px'
+    button2.style.color = 'pink'
+
+    myDiv.appendChild(button2);
+    })
+document.getElementsByTagName('body')[0].appendChild(myDiv);
+
+// events
+
+
+const btn = document.getElementsByTagName('button')[0];
+const btn2 = document.getElementsByTagName('button')[1];
+const btn_sum = document.getElementsByTagName('button2')[0];
+
+
+
+btn.onclick = (event) =>{
+    event.target.innerText = `Додати в друзі ${1+i}`;
+    btn.addEventListener('click', function () {
+        // btn_sum+ btn, this.disabled=true;
+
+    });
+}
+
+
+
+btn_sum.onclick = (event) =>{
+    event.target.innerText = `Кiлькiсть друзiв ${Math.random(1,100)*100}`;
+};
+
+
+
+
+let colors = ['yellow','green','purple']
+
+btn2.onclick = (event) => {
+    let newDiv = document.createElement('div');
+    newDiv.style.background = colors[document.getElementsByClassName('dynamic').length % 3];
+    newDiv.innerText = 'My dinamic txt'
+    newDiv.className = 'dynamic';
+    document.getElementsByClassName('dynamic').length
+    document.getElementsByTagName('body')[0].appendChild(newDiv);
+};
+
