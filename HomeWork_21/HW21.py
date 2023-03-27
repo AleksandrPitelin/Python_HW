a69df070fb3dac26354b6e255465d0f9ca987c53
AleksandrPitelin <partsua@ukr.net>
2023-03-27 14:37:40 +0300

    add files HW21

--- HomeWork_21/HW21.py
+++ HomeWork_21/HW21.py
@@ -2,7 +2,7 @@ import sqlite3
 
 db = sqlite3.connect("/Users/AleksandrPitelin/PythonProject/PythonProject/HomeWork_21/HW21.sqlite")
 c = db.cursor()
-# print(db)
+
 
 c.execute(""" CREATE TABLE IF NOT EXISTS users (
     id integer PRIMARY KEY AUTOINCREMENT,
@@ -11,6 +11,37 @@ c.execute(""" CREATE TABLE IF NOT EXISTS users (
     age integer
 )
 """)
+#
+c.execute("SELECT * FROM users WHERE age > 30;")
+rows = c.fetchall()
+for row in rows:
+    print(row)
+
+#--------------------------#
+
+
+c.execute("SELECT COUNT(*) FROM users WHERE age > 30;")
+count = c.fetchone()[0]
+print(count)
+
+
+#---------------------------#
+
+
+
+c.execute("SELECT age, COUNT(*) AS users FROM users GROUP BY age ORDER BY age;")
+rows = c.fetchall()
+for row in rows:
+    print(row)
+
+#---------------------------#
+
+c.execute("SELECT age, COUNT(*) AS users FROM users GROUP BY age ORDER BY users DESC,age ASC;")
+rows = c.fetchall()
+for row in rows:
+    print(row)
+
+
 
 
 db.commit()
